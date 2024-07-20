3e61de3392213dbc07527c32bbf1c09aa9868e20
宋厚锦 <songhoujin@mediinfo.com.cn>
2024-07-20 14:57:14 +0800

    提交 , 周日作业

---
+++ 00CMakePractices/01CMakeTutorial-Demo/CMakeLists-1-Test/test01.cxx
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "test01.hxx"
+
+using namespace std;
+
+
+void print_hello(){
+	cout << "hello, world." << endl;
+}
+
+
+int main(){
+	print_hello();
+	cout << "===== end =====" << endl;
+	return 0;
+}
